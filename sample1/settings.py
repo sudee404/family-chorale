9019d41a37c49089acb8eea24acd85c19fc2e237
sudee404 <sudaise18@gmail.com>
2022-02-15 13:09:10 +0300

    setting debug

--- sample1/settings.py
+++ sample1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
